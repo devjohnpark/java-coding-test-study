7b1561c4d254e3e06a9777ba2436284afd0573ee
jiruen <kjr2699@naver.com>
2025-09-13 23:40:56 +0900

    [LTC] 743 40m

---
+++ rosie98/shortest_path/LTC_743.java
@@ -0,0 +1,68 @@
+class Node {
+    int target;
+    int time;
+
+    public Node(int target, int time) {
+        this.target= target;
+        this.time = time;
+    }
+}
+
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        List<List<Node>> nodes = new ArrayList<>();
+
+        for(int i=0; i<=n; i++) {
+            nodes.add(new ArrayList<>());
+        }
+
+        for(int i=0; i<times.length; i++) {
+            nodes.get(times[i][0]).add(new Node(times[i][1], times[i][2]));
+        }
+        
+        return dijkstra(times, n, k, nodes);
+    }
+
+    public int dijkstra(int[][] times, int n, int k, List<List<Node>> nodes) {
+        boolean[] visited = new boolean[n+1]; 
+        int[] dist = new int[n+1];
+
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> {
+            return o1.time - o2.time;
+        });
+
+        pq.offer(new Node(k, 0));
+        dist[k] = 0;
+
+        while(!pq.isEmpty()) {
+            int nowNode= pq.poll().target;
+
+            if (visited[nowNode]) {
+                continue;
+            }
+
+            visited[nowNode] = true;
+
+            for(Node node : nodes.get(nowNode)) {    
+                if (dist[node.target] > dist[nowNode] + node.time) {
+                    dist[node.target] = dist[nowNode] + node.time;
+
+                    pq.offer(new Node(node.target, dist[node.target]));
+                }
+            }
+        }
+
+        int answer = 0;
+        for(int i=1; i<=n; i++) {
+            if (dist[i] == Integer.MAX_VALUE) {
+                return -1;
+            } 
+
+            answer = Math.max(answer, dist[i]);
+        }
+
+        return answer;
+    }
+}
