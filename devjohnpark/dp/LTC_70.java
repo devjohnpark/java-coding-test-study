0fba8c1e627d23c10afa9a8496b96e65917be5bf
devjohnpark <devjohnpark@gmail.com>
2025-09-26 08:51:37 +0900

    [LTC] 70 11m

---
+++ devjohnpark/dp/LTC_70.java
@@ -0,0 +1,9 @@
+class Solution {
+    int[] d = new int[46];     
+    public int climbStairs(int n) {
+        if (n == 1) return 1; 
+        if (n == 2) return 2; 
+        if(d[n] != 0) return d[n];
+        return d[n] = climbStairs(n-1) + climbStairs(n-2);
+    } 
+}
